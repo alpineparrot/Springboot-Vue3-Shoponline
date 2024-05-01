87f747b4ce2d11ecd1a8f21e1b89b66da31672d5
sofie <sopurplerp@gmail.com>
2024-05-01 21:44:51 +0900

    springboot+vue3 frontend

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="text-body-secondary py-5">
+    <div class="container">
+      <p class="float-end mb-1">
+        <a href="#">عد إلى الأعلى</a>
+      </p>
+      <p class="mb-1">مثال الألبوم هو © Bootstrap ، ولكن يرجى تنزيله وتخصيصه لنفسك!</p>
+      <p class="mb-0">جديد على Bootstrap؟ <a href="/"> تفضل بزيارة الصفحة الرئيسية </a> أو اقرأ <a href="/docs/5.3/getting-started/introduction/ "> دليل البدء </a>.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+</style>
