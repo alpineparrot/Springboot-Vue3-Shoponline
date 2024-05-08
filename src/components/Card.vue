8e395b8e25ebd2b88852260154c918304c75da31
alpineparrot <102452709+alpineparrot@users.noreply.github.com>
2024-05-08 19:48:41 +0900

    Add files via upload

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="card shadow-sm">
+     <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}"></span>
+      <div class="card-body">
+        <p class="card-text">{{ item.name }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Get this</button>
+          </div>
+          <small class="text-body-secondary">{{ item.price }}euros</small>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props:{
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+.card .img{
+  display:inline-block;
+  width:100%;
+  height:250px;
+  background-size: cover;
+  background-position: center;
+}
+</style>
